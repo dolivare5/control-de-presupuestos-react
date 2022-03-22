311969375bebd1ade4d646a91486ee4691c60785
dolivare5 <deiverolivares2@gmail.com>
2022-03-22 04:16:54 -0500

    Implemente una funcionalidad para convertir edl presupuesto al formato de dolar estadounidense

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const ControlPresupuesto = ({presupuesto}) => {
+
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
+
     return (
         <div className="contenedor-presupuesto contenedor sobra dos-columnas">
             <div>
@@ -8,7 +16,9 @@ const ControlPresupuesto = ({presupuesto}) => {
             </div>
 
             <div className="contenido-presupuesto">
-                <p><span>Presupuesto: </span>  ${presupuesto}</p>
+                <p><span>Presupuesto: </span>  {formatearCantidad(presupuesto)}</p>
+                <p><span>Presupuesto: </span>  {formatearCantidad(0)}</p>
+                <p><span>Presupuesto: </span>  {formatearCantidad(0)}</p>
             </div>
         </div>
     );
