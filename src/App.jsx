1aa1f756ae489397426bc80ac3975376b0a42f59
dolivare5 <deiverolivares2@gmail.com>
2022-03-22 03:48:04 -0500

    Implemente una función para validar que solo se permita el ingreso de números y a partir de alli a través de un state redirecionarlo a otro componente con erl detalle del presupuesto.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Header from "./components/Header";
 function App() {
     const [count, setCount] = useState(0);
     const [presupuesto, setPresupuesto] = useState(0);
+    const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
     return (
         <div>
             <Header
                 presupuesto = {presupuesto}
                 setPresupuesto = {setPresupuesto}
+                isValidPresupuesto = {isValidPresupuesto}
+                setIsValidPresupuesto = {setIsValidPresupuesto}
             />
         </div>
     )
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,24 +1,38 @@
+import { useState } from 'react';
+import Mensaje from "./Mensaje";
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto, setIsValidPresupuesto }) => {
+    const [mensaje, setMensaje] = useState('');
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if(!Number(presupuesto)|| Number(presupuesto <= 0)){
+            setMensaje('No es un presupuesto valido')
+        }else{
+            setMensaje('')
+            setIsValidPresupuesto(true)
+;        }
+    }
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
     return (
         <div>
             <div className="contenedor-presupuesto contenedor sombra">
-                <form className="formulario">
+                <form onSubmit={handlePresupuesto} className="formulario">
                     <div className="campo">
                         <label>Definir Presupuesto</label>
                         <input
                             className="nuevo-presupuesto"
-                            type="text"
+                            type="number"
                             placeholder="Agregue aquí su Presupuesto"
                             value={presupuesto}
-                            onChange={ e => setPresupuesto(e.target.value) }
+                            onChange={ e => setPresupuesto(Number(e.target.value)) }
                         />
                     </div>
 
                     <input
                         type="submit"
-                        value="Registrar Presupuesto"
+                        value="Registrar-Presupuesto"
                     />
+
+                    {mensaje && <Mensaje tipo={"error"}>{mensaje}</Mensaje> }
                 </form>
             </div>
         </div>
