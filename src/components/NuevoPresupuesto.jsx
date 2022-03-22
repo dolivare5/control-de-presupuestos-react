28179b64404a0484b053961bfb642eb862d75b43
dolivare5 <deiverolivares2@gmail.com>
2022-03-21 19:31:14 -0500

    Importé el componente Header.jsx en App.jsx y tambien defini la estructura basica del formulario con las clases de estilos correspondientes a través del componente NuevoPresupuesto.jsx el cual importo desde Header,jsx.

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -2,7 +2,23 @@
 const NuevoPresupuesto = () => {
     return (
         <div>
+            <div className="contenedor-presupuesto contenedor sombra">
+                <form className="formulario">
+                    <div className="campo">
+                        <label>Definir Presupuesto</label>
+                        <input
+                            className="nuevo-presupuesto"
+                            type="text"
+                            placeholder="Agreue aquí su Presupuesto"
+                        />
+                    </div>
 
+                    <input
+                        type="submit"
+                        value="Registrar Presupuesto"
+                    />
+                </form>
+            </div>
         </div>
     );
 };
